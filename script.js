1ff72c3720c084d1dbc9fc8beec6bedb27dcca65
Minh Le <xuanminhle1611@gmail.com>
2022-05-09 17:13:16 +0700

    countdown timer final commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const newYears = "01/01/2023";
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+function countdown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+countdown();
+
+setInterval(countdown, 1000);
